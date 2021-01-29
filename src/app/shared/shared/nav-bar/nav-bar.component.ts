ae32aa143199c2015291f5a8cdc393eddfc2fe1a
jluctamar <jeffly.luctamar@gmail.com>
2021-01-28 20:52:33 -0500

    built out the nav-bar component

--- src/app/shared/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nav-bar',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  
+  title = 'Christ Connection';
 
-  ngOnInit(): void {
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  
+  // routing to the home page
+  goToHome(){
+    this.router.navigate(['home']);
+  }
+
+  // routing to the gallery page
+  goToFellowship(){
+    this.router.navigate(['fellowship']);
+  }
+
+  // routing to bible study page
+  goToBibleStudy(){
+    this.router.navigate(['bible-study']);
+  }
+
+  // routing to prayer page
+  goToPrayerNight(){
+    this.router.navigate(['prayer']);
+  }
+
+  // routing to about page
+  goToAboutUs(){
+    this.router.navigate(['about']);
+  }
+  
+
+  //function takes in information about the currently selected tab and routes to the corresponding page 
+  tabChange( tabChangeEvent: MatTabChangeEvent){
+
+    switch(tabChangeEvent.index){
+      case 0: this.goToHome(); break;
+      case 1: this.goToBibleStudy(); break;
+      case 2: this.goToFellowship(); break;
+      case 3: this.goToPrayerNight(); break;
+      case 4: this.goToAboutUs(); break;
+    }
   }
 
 }
