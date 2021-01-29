0e553d62c8da42fd7fc1027c48bd08a1f22f0019
jluctamar <jeffly.luctamar@gmail.com>
2021-01-28 20:19:38 -0500

    built out footer component, implemented first mixin

--- src/app/shared/shared/footer/footer.component.ts
+++ src/app/shared/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { passage, VerseService } from 'src/app/services/verseService';
 
 @Component({
   selector: 'footer',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  passage: passage = null;
+  test: string = "";
+  passageSelectionArr = ["Genesis 3:20", "Genesis 1:1-3", "Genesis 2:7", "1 John 3:4", "Ephesians 2:10", "2 Timothy 3:16", "Psalms 119:9", "1 timothy 1:15",
+    "2 Corinthians 3:17", "Isaiah 55:6-7", "John 3:16", "John 1:1", "John 1:12", "John 3:5", "Matthew 11:28",];
 
-  ngOnInit(): void {
+  constructor(private verseService: VerseService) {}
+
+  ngOnInit() {
+    this.onDisplayPassage()
   }
 
+  onDisplayPassage() {
+    //this.passage = this.verseService.displayPassage();
+
+    let verse = this.passageSelectionArr[Math.floor(Math.random() * this.passageSelectionArr.length)];
+    this.verseService.getPassage(verse).subscribe((passage) => {
+      // format the string by removing unwanted characters 
+      this.test = JSON.stringify(passage.passages)
+        .replace("[", "")
+        .replace("]", "")
+        .replace(/\\n/g, ""); // removes newline character
+    });
+  }
+
+  displayRandomPassage() { // NOTE this API call is limited to 10 calls per hour.
+    //this.passage = this.verseService.displayPassage();
+    this.verseService.getRandomPassage().subscribe((passage) => {
+      // format the string by removing unwanted characters 
+      this.test = JSON.stringify(passage.contents.verse)
+    });
+  }
 }
