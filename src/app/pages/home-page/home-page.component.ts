5f6bcd77ad4ca4aa170627a571ef0190017e922f
jluctamar <jeffly.luctamar@gmail.com>
2021-01-30 23:23:48 -0500

    updated Home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VerseService } from 'src/app/services/verseService';
 
 @Component({
   selector: 'home-page',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private verseService: VerseService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
   }
 
+  goToAboutUs(){
+    this.router.navigate(['about']);
+  }
 }
