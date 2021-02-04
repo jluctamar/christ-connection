ca9a967acfc5fcf5f7004b9ff0f4d7dc58497bd6
jluctamar <jeffly.luctamar@gmail.com>
2021-02-04 01:30:58 -0500

    began work on the Bible study page

--- src/app/pages/bible-study/bible-study.component.ts
+++ src/app/pages/bible-study/bible-study.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BibleStudyComponent implements OnInit {
 
+  christConnection = `Jesus' mission was to seek and save the lost. He is the One who seeks us while we 
+  are undeserving sinners, saves us according to His mercy through His work on the cross,
+  and delights in our hearfelt expressions of repentance.`
+
   constructor() { }
 
   ngOnInit(): void {
