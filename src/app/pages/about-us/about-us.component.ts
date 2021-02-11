d44b4d653163f8030a7f50e04fe8202e5e4e546d
jluctamar <jeffly.luctamar@gmail.com>
2021-02-10 21:11:12 -0500

    added fade in animation within the about page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent implements OnInit, OnInit, OnDestroy {
+  viewportHeight = window.innerHeight || document.documentElement.clientHeight;
 
-  constructor() { }
+  viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true); // third parameter
+    console.log('viewportHeight: ', this.viewportHeight);
+    console.log('viewportWidth: ', this.viewportWidth);
   }
 
+  scroll = (event: any): void => {
+    // Here scroll is a variable holding the anonymous function
+    // this allows scroll to be assigned to the event during onInit
+    // and removed onDestroy
+    // To see what changed:
+    const scrollVal = event.srcElement.scrollTop;
+    const parentCoords = document.querySelector('.stewards') as HTMLElement; // gets the position of the parent Div
+
+    const firstCard = document.querySelector('.card-one') as HTMLElement;
+    const secCard = document.querySelector('.card-two') as HTMLElement;
+    const thirdCard = document.querySelector(' .card-three') as HTMLElement;
+    const fourthCard = document.querySelector('.card-four') as HTMLElement;
+
+    console.group('OffsetTop');
+    console.log(
+      'Scroll Value(Parent):',
+      scrollVal,
+      '----',
+      parentCoords.offsetTop
+    );
+    console.log(
+      'Scroll Value(1):',
+      scrollVal,
+      '----',
+      firstCard.offsetTop,
+      '----',
+      firstCard.offsetTop - parentCoords.offsetTop
+    );
+    console.log(
+      'Scroll Value:(2)',
+      scrollVal,
+      '----',
+      secCard.offsetTop,
+      '----',
+      secCard.offsetTop - parentCoords.offsetTop
+    );
+    console.log(
+      'Scroll Value:(3)',
+      scrollVal,
+      '----',
+      thirdCard.offsetTop,
+      '----',
+      thirdCard.offsetTop - parentCoords.offsetTop
+    );
+    console.log(
+      'Scroll Value:(4)',
+      scrollVal,
+      '----',
+      fourthCard.offsetTop,
+      '----',
+      fourthCard.offsetTop - parentCoords.offsetTop
+    );
+    console.groupEnd();
+    console.group('OffsetParent');
+    console.log('Scroll Value(1):', scrollVal, '----', firstCard.offsetParent);
+    console.log('Scroll Value:(2)', scrollVal, '----', secCard.offsetParent);
+    console.log('Scroll Value:(3)', scrollVal, '----', thirdCard.offsetParent);
+    console.log('Scroll Value:(4)', scrollVal, '----', fourthCard.offsetParent);
+    console.groupEnd();
+    console.log('---------------------------------');
+
+    // determines when the about cards will fade in, in relation to cards position on the screen
+    if ((scrollVal > (firstCard.offsetTop - parentCoords.offsetTop)) && (scrollVal < (secCard.offsetTop - parentCoords.offsetTop))) {
+      firstCard.style.transform = 'translateX(0%)';
+      firstCard.style.opacity = '1';
+    }
+    if ((scrollVal > (secCard.offsetTop - parentCoords.offsetTop)) && (scrollVal < (thirdCard.offsetTop - parentCoords.offsetTop))) {
+      secCard.style.transform = 'translateX(0%)';
+      secCard.style.opacity = '1';
+
+    }
+    if ((scrollVal > (thirdCard.offsetTop - parentCoords.offsetTop)) && (scrollVal < (fourthCard.offsetTop - parentCoords.offsetTop))) {
+      thirdCard.style.opacity = '1';
+      thirdCard.style.transform = 'translateX(0%)';
+
+    }
+    if (scrollVal > (fourthCard.offsetTop - parentCoords.offsetTop)) {
+      fourthCard.style.transform = 'translateX(0%)';
+      fourthCard.style.opacity = '1';
+
+    }
+  };
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
 }
--- src/app/pages/bible-study/bible-study.component.ts
+++ src/app/pages/bible-study/bible-study.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ShutterOutAnimation } from 'src/app/shared/shared/animations';
 
@@ -8,7 +8,7 @@ import { ShutterOutAnimation } from 'src/app/shared/shared/animations';
   styleUrls: ['./bible-study.component.scss'],
   animations: [ShutterOutAnimation],
 })
-export class BibleStudyComponent implements OnInit {
+export class BibleStudyComponent implements OnInit, OnDestroy {
   christConnection = `Jesus' mission was to seek and save the lost. He is the One who seeks us while we 
   are undeserving sinners, saves us according to His mercy through His work on the cross,
   and delights in our hearfelt expressions of repentance.`;
@@ -19,12 +19,13 @@ export class BibleStudyComponent implements OnInit {
   openLessonView = false;
   lessonURL: SafeUrl;
 
-  constructor(private domSanitizer : DomSanitizer) {}
+  constructor(private domSanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     // Todo: Disable the zoom connect buttons on the basis of the designated meeting date and time
     // meaning the buttons will be enabled for the two hour time slots on the given meeting day
-    window.addEventListener('scroll', this.scroll, true); //third parameter
+
+    window.addEventListener('scroll', this.scroll, true); // third parameter
   }
 
   scroll = (event: any): void => {
@@ -33,33 +34,36 @@ export class BibleStudyComponent implements OnInit {
     // and removed onDestroy
     // To see what changed:
     const scrollVal = event.srcElement.scrollTop;
-    var parentCoords = document.querySelector('.past-lessons-container')as HTMLElement; // gets the position of the parent Div
+    const parentCoords = document.querySelector(
+      '.past-lessons-container'
+    ) as HTMLElement; // gets the position of the parent Div
 
-    let target = document.querySelector('.lesson-wrapper') as HTMLElement;
-    let scrollRate = (scrollVal-parentCoords.offsetTop)/parentCoords.offsetTop * 45
-    if(scrollVal >= parentCoords.offsetTop){
-      target.style.transform = 'translateX(-'+scrollRate+'%)'
+    const target = document.querySelector('.lesson-wrapper') as HTMLElement;
+    const scrollRate =
+      ((scrollVal - parentCoords.offsetTop) / parentCoords.offsetTop) * 45;
+    if (scrollVal >= parentCoords.offsetTop) {
+      target.style.transform = 'translateX(-' + scrollRate + '%)';
     } else {
-      target.style.transform = 'translateX(0%)'
+      target.style.transform = 'translateX(0%)';
     }
-  };
+  }
 
-
-  onZoomConnect() {
+  onZoomConnect(): void {
     document.location.href = this.zoomLink;
   }
 
-  onViewLesson(urlLocation: string) {
+  onViewLesson(urlLocation: string): void {
     this.openLessonView = true;
-    this.lessonURL = this.domSanitizer.bypassSecurityTrustResourceUrl(urlLocation);
-    console.log("##################", this.lessonURL)
+    this.lessonURL = this.domSanitizer.bypassSecurityTrustResourceUrl(
+      urlLocation
+    );
   }
 
-  onClosePassageDisplay() {
+  onClosePassageDisplay(): void {
     this.openLessonView = false;
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     window.removeEventListener('scroll', this.scroll, true);
   }
 }
