f3c2b29b971ac1b3e3fcea6dd83485dec7ffc9de
jluctamar <jeffly.luctamar@gmail.com>
2021-02-11 20:37:36 -0500

    updating footer responsiveness for small screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
-import { SlideInOutAnimation } from './shared/shared/animations';
+import { SlideInOutAnimation, SlideUpDownAnimation } from './shared/shared/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [ SlideInOutAnimation]
+  animations: [ SlideInOutAnimation, SlideUpDownAnimation]
 })
 export class AppComponent {
   title = 'christ-connection';
-  isVisible = false; 
+  isVisible = false;
 
-  onDisplayPassageEvent() {
-    if(!this.isVisible) {
-      this.isVisible = true
+  // gets the screen size for footer trasition
+  // limitation: if screen size changes variable only updates after refresh
+  isSmallScreen =  (window.innerWidth || document.documentElement.clientWidth) <= 480;
+
+  onDisplayPassageEvent(): void {
+    if (!this.isVisible) {
+
+      this.isVisible = true;
     }
     // must account for alreaady open scenario 
       // should the nav-bar button prompt for another verse?
   }
 
-  onClosePassageDisplayEvent() {
+  onClosePassageDisplayEvent(): void {
       this.isVisible = false;
   }
 }
--- src/app/shared/shared/animations.ts
+++ src/app/shared/shared/animations.ts
@@ -1,35 +1,73 @@
-import { trigger, state, style, transition,
-    animate, group, query, stagger, keyframes
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  group,
+  query,
+  stagger,
+  keyframes,
 } from '@angular/animations';
 
 // https://stackoverflow.com/questions/47248898/angular-4-5-6-7-simple-example-of-slide-in-out-animation-on-ngif
 
 export const SlideInOutAnimation = [
-    trigger('slideInOut', [
-      transition(':enter', [
-        style({transform: 'translateX(-100%)', opacity: '0'}),
-        animate('900ms ease', style({transform: 'translateX(0%)', opacity: '1'}))
-      ]),
-      transition(':leave', [
-        animate('800ms ease-out', style({transform: 'translateX(-100%)', opacity: '0'}))
-      ])
-    ])
-  ]
+  trigger('slideInOut', [
+    transition(':enter', [
+      style({ transform: 'translateX(-100%)', opacity: '0' }),
+      animate(
+        '900ms ease',
+        style({ transform: 'translateX(0%)', opacity: '1' })
+      ),
+    ]),
+    transition(':leave', [
+      animate(
+        '800ms ease-out',
+        style({ transform: 'translateX(-100%)', opacity: '0' })
+      ),
+    ]),
+  ]),
+];
 
 export const SlideUpAnimation = [
-    trigger('slideUp', [
-      transition(':enter', [
-        style({transform: 'translateY(-100%)', opacity: '0'}),
-        animate('700ms ease-out', style({transform: 'translateX(0%)', opacity: '1'}))
-      ])
-    ])
-  ]
+  trigger('slideUp', [
+    transition(':enter', [
+      style({ transform: 'translateY(-100%)', opacity: '0' }),
+      animate(
+        '700ms ease-out',
+        style({ transform: 'translateX(0%)', opacity: '1' })
+      ),
+    ]),
+  ]),
+];
 
-  export const ShutterOutAnimation = [
-    trigger('shutterOut', [
-      transition(':enter', [
-        style({transform: 'scalex(0)', opacity: '0'}),
-        animate('700ms ease-out', style({transform: 'scaleX(1)', opacity: '.8'}))
-      ])
-    ])
-  ]
+export const SlideUpDownAnimation = [
+  trigger('slideUpDown', [
+    transition(':enter', [
+      style({ transform: 'translateY(100%)', opacity: '0' }),
+      animate(
+        '1000ms ease-out',
+        style({ transform: 'translateY(0%)', opacity: '1' })
+      ),
+    ]),
+    transition(':leave', [
+      animate(
+        '800ms ease-out',
+        style({ transform: 'translateY(100%)', opacity: '0' })
+      ),
+    ]),
+  ]),
+];
+
+export const ShutterOutAnimation = [
+  trigger('shutterOut', [
+    transition(':enter', [
+      style({ transform: 'scalex(0)', opacity: '0' }),
+      animate(
+        '700ms ease-out',
+        style({ transform: 'scaleX(1)', opacity: '.8' })
+      ),
+    ]),
+  ]),
+];
